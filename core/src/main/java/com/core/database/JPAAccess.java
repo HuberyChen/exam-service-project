76f22784f1ea6d977050d89096caf06f70db2d2b
hubery.chen <hubery.chen@suryani.cn>
2014-07-01 13:02:53 +0800

    exam-service

---
+++ core/src/main/java/com/core/database/JPAAccess.java
@@ -0,0 +1,112 @@
+package com.core.database;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author neo
+ */
+public class JPAAccess {
+
+    private EntityManager entityManager;
+
+    public <T> T get(Class<T> entityClass, Object id) {
+        return entityManager.find(entityClass, id);
+    }
+
+    public void save(Object entity) {
+        entityManager.persist(entity);
+    }
+
+    public void update(Object entity) {
+        entityManager.merge(entity);
+    }
+
+    public void delete(Object entity) {
+        entityManager.remove(entity);
+    }
+
+    public void refresh(Object entity) {
+        entityManager.refresh(entity);
+    }
+
+    public void detach(Object entity) {
+        entityManager.detach(entity);
+    }
+
+    public <T> List<T> find(CriteriaQuery<T> query) {
+        return entityManager.createQuery(query).getResultList();
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> List<T> find(String queryString, Map<String, Object> params) {
+        Query query = entityManager.createQuery(queryString);
+        if (params != null)
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                query.setParameter(entry.getKey(), entry.getValue());
+            }
+        return query.getResultList();
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> List<T> find(String queryString, Map<String, Object> params, int offset, int fetchSize) {
+        Query query = entityManager.createQuery(queryString);
+        if (params != null) {
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                query.setParameter(entry.getKey(), entry.getValue());
+            }
+        }
+        query.setFirstResult(offset);
+        query.setMaxResults(fetchSize);
+        return query.getResultList();
+    }
+
+
+    public <T> T findUniqueResult(CriteriaQuery<T> query) {
+        List<T> results = entityManager.createQuery(query).getResultList();
+        return getUniqueResult(results);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T findUniqueResult(String queryString, Map<String, Object> params) {
+        Query query = entityManager.createQuery(queryString);
+        if (params != null)
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                query.setParameter(entry.getKey(), entry.getValue());
+            }
+        List<T> results = query.getResultList();
+        return getUniqueResult(results);
+    }
+
+    private <T> T getUniqueResult(List<T> results) {
+        if (results.isEmpty()) return null;
+        if (results.size() > 1) {
+            throw new NonUniqueResultException("result returned more than one element, returnedSize=" + results.size());
+        }
+        return results.get(0);
+    }
+
+    public int update(String queryString, Map<String, Object> params) {
+        Query query = entityManager.createQuery(queryString);
+        if (params != null)
+            for (Map.Entry<String, Object> entry : params.entrySet()) {
+                query.setParameter(entry.getKey(), entry.getValue());
+            }
+        return query.executeUpdate();
+    }
+
+    public CriteriaBuilder criteriaBuilder() {
+        return entityManager.getCriteriaBuilder();
+    }
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+}
---
+++ core/src/main/java/com/core/platform/web/filter/PlatformFilter.java
@@ -0,0 +1,31 @@
+package com.core.platform.web.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * @author neo
+ */
+public class PlatformFilter implements Filter {
+
+    private static final String UTF_8 = "UTF-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding(UTF_8);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
